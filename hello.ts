38526955f1f7174c57e14530cb319ce1355dd05b
James Turner <james@calminferno.net>
2021-07-05 14:21:06 -0400

    Get a little more complicated

--- hello.ts
+++ hello.ts
@@ -2,6 +2,19 @@ import { Application } from "https://deno.land/x/oak@v7.7.0/mod.ts";
 
 const app = new Application();
 
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
 app.use((ctx) => {
   ctx.response.body = "Hello deno!";
 });

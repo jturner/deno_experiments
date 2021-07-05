070fc8fbee32fab43774837a4a25c27bc98d0ad6
James Turner <james@calminferno.net>
2021-07-05 13:51:17 -0400

    Testing github action

--- hello.ts
+++ hello.ts
@@ -3,7 +3,7 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 
 app.use((ctx) => {
-  ctx.response.body = "Hello world!";
+  ctx.response.body = "Hello deno!";
 });
 
 addEventListener("fetch", app.fetchEventHandler());

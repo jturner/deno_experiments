23b0de2c764fc74535d31b57a9594f94901edcf7
James Turner <james@calminferno.net>
2021-12-21 15:05:19 -0500

    Trying a new framework

---
+++ sift.jsx
@@ -0,0 +1,44 @@
+/** @jsx h */
+import {
+  h,
+  jsx,
+  serve,
+  serveStatic,
+} from "https://deno.land/x/sift@0.4.2/mod.ts";
+
+const App = () => (
+  <html>
+    <head>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    </body>
+  </html>
+);
+
+const NotFound = () => (
+  <html>
+  <head>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div>
+      <h1>Page not found</h1>
+    </div>
+  </body>
+</html>
+);
+
+serve({
+  "/": () => jsx(<App />),
+  404: () => jsx(<NotFound />, { status: 404 }),
+  "/style.css": serveStatic("style.css", {
+    baseUrl: import.meta.url,
+    intervene: (request, response) => {
+      return response;
+    },
+  }),
+});
